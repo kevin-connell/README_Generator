efd426d10d5e0b63f60727c1d7ebe8e6c59d6b14
FileOtter <kevin.t.connell@gmail.com>
2020-09-24 12:33:01 -0400

    added questions

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'username',
+        message: "What is your GitHub username?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is your email address?",
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is the title of your project?",
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Provide a description of your project",
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: "What is your GitHub username?",
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license would you like?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'intall',
+        message: "What command should be run to install dependencies?",
+        default: function () {
+            return 'npm i';
+        },
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: "What command should be run to run tests?",
+        default: function () {
+            return 'npm test';
+        },
+    },
+    {
+        type: 'input',
+        name: 'use',
+        message: "What does the user need to know about using the repo?",
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: "What does the user need to know about contributing the repo?",
+    },
 ];
 
 // function to write README file
